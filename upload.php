b07eeaa5a787a1eb6c50e980f5b9293223bfbaf7
sizukutamago <tanaka@tam-bourine.co.jp>
2019-11-24 17:57:57 +0900

    ffmpeg

--- upload.php
+++ upload.php
@@ -7,7 +7,11 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 
 $file = $_FILES['audio'];
 
+$fileName = 'upload_' . time();
+
 move_uploaded_file(
     $file['tmp_name'],
-    './uploads/test.wav'
+    './uploads/' . $fileName . '.wav'
 );
+
+exec('ffmpeg -i ./uploads/' . $fileName . '.wav -vn -ac 2 -ar 44100 -ab 128k -acodec wmav2 -f asf ./uploads/' . $fileName . '.mp3');
